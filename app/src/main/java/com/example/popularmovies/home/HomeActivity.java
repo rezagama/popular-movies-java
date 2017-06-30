d2aa2426c0b71d6b3ee0e01540e973dc9c5c5880
rezagama <reza.hidayat@go-jek.com>
2017-06-30 17:51:36 +0700

    [REZA] Refactor main activity to home activity

--- app/src/main/java/com/example/popularmovies/home/HomeActivity.java
+++ app/src/main/java/com/example/popularmovies/home/HomeActivity.java
@@ -1,13 +1,15 @@
-package com.example.popularmovies;
+package com.example.popularmovies.home;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.popularmovies.R;
+
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
     }
 }
