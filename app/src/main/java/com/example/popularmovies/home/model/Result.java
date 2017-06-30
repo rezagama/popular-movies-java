58a1858de41cba6c816c4e609e105c97dd4cbf27
rezagama <reza.hidayat@go-jek.com>
2017-06-30 23:09:21 +0700

    [REZA] Implement parcelable on Result model

--- app/src/main/java/com/example/popularmovies/home/model/Result.java
+++ app/src/main/java/com/example/popularmovies/home/model/Result.java
@@ -1,5 +1,8 @@
 package com.example.popularmovies.home.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.SerializedName;
 
 import java.util.ArrayList;
@@ -9,7 +12,7 @@ import java.util.List;
  * Created by rezagama on 6/30/17.
  */
 
-public class Result {
+public class Result implements Parcelable {
     @SerializedName("vote_count")
     public int voteCount;
     @SerializedName("id")
@@ -38,4 +41,57 @@ public class Result {
     public String overview;
     @SerializedName("release_date")
     public String releaseDate;
+
+    private Result(Parcel in) {
+        voteCount = in.readInt();
+        id = in.readInt();
+        video = in.readByte() != 0;
+        voteAverage = in.readFloat();
+        title = in.readString();
+        popularity = in.readFloat();
+        posterPath = in.readString();
+        originalLanguage = in.readString();
+        originalTitle = in.readString();
+        genreIds = new ArrayList<>();
+        in.readList(genreIds, Integer.class.getClassLoader());
+        backdropPath = in.readString();
+        adult = in.readByte() != 0;
+        overview = in.readString();
+        releaseDate = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel out, int i) {
+        out.writeInt(voteCount);
+        out.writeInt(id);
+        out.writeByte((byte) (video ? 1 : 0));
+        out.writeFloat(voteAverage);
+        out.writeString(title);
+        out.writeFloat(popularity);
+        out.writeString(posterPath);
+        out.writeString(originalLanguage);
+        out.writeString(originalTitle);
+        out.writeList(genreIds);
+        out.writeString(backdropPath);
+        out.writeByte((byte) (video ? 1 : 0));
+        out.writeString(overview);
+        out.writeString(releaseDate);
+    }
+
+    public static final Parcelable.Creator<Result> CREATOR = new Parcelable.Creator<Result>() {
+        @Override
+        public Result createFromParcel(Parcel source) {
+            return new Result(source);
+        }
+
+        @Override
+        public Result[] newArray(int size) {
+            return new Result[size];
+        }
+    };
 }
